14bead8720625f978411e331f0bc3bd977e09364
Zack Stayman <z.stayman@gmail.com>
2014-03-09 22:57:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BoardGamer::Application.config.secret_key_base = '45ff506ababb2355f148ad6d9fae4920be1df567e27e202f7f619c300f569079b91ffec70beb562a8e5acbe79423c497451a4f339659a6f2df81baf9880decb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BoardGamer::Application.config.session_store :cookie_store, key: '_board_gamer_session'
